d2184ba7d8df48d8f643e8851566c26e6baadcb2
brodieheg <brodieheg@gmail.com>
2024-07-09 14:11:12 -0400

    added enviornment variables, set up Express boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import fetch from "node-fetch";
+import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
 const response = await fetch("https://github.com/");
 const text = await response.text();
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
 console.log(text);
+console.log(PORT);
+console.log(process.env.STATUS);
+
+// todo: set up express
